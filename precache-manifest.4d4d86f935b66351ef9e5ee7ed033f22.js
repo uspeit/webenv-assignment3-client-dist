6b5ae11d29cbc6b236f45d41736ffa6973646bb3
Eitan Fedenko <eitan.lp@gmail.com>
2020-07-20 20:13:53 +0300

    deploy

---
+++ precache-manifest.4d4d86f935b66351ef9e5ee7ed033f22.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80cccbd90d5a58e8c17",
+    "url": "/webenv-assignment3-client-dist/css/app.e33d5a6f.css"
+  },
+  {
+    "revision": "917a1ff8ab620f0beb45",
+    "url": "/webenv-assignment3-client-dist/css/chunk-0b12526b.deeb5be3.css"
+  },
+  {
+    "revision": "396b3e71d6f0275e1ebb",
+    "url": "/webenv-assignment3-client-dist/css/chunk-1ddd5a8a.8789bfe3.css"
+  },
+  {
+    "revision": "56013ea6b697f17ec27a",
+    "url": "/webenv-assignment3-client-dist/css/chunk-296a3085.4c63a3d9.css"
+  },
+  {
+    "revision": "18e133d8c9cc570550b1",
+    "url": "/webenv-assignment3-client-dist/css/chunk-3615582d.36f35cce.css"
+  },
+  {
+    "revision": "f3d3e84b0153dcd0485c",
+    "url": "/webenv-assignment3-client-dist/css/chunk-5f9f11b2.548fe094.css"
+  },
+  {
+    "revision": "48c4be7523ddddec6a9c",
+    "url": "/webenv-assignment3-client-dist/css/chunk-e9e4a9b2.a9a49200.css"
+  },
+  {
+    "revision": "8d097d60ace2838f3ba6",
+    "url": "/webenv-assignment3-client-dist/css/chunk-vendors.5d46be18.css"
+  },
+  {
+    "revision": "ae8d1f9b77733dbe2665ccf84bae91f5",
+    "url": "/webenv-assignment3-client-dist/img/background.ae8d1f9b.jpg"
+  },
+  {
+    "revision": "fb0321d0bdf018acebdb4b6895097400",
+    "url": "/webenv-assignment3-client-dist/index.html"
+  },
+  {
+    "revision": "c80cccbd90d5a58e8c17",
+    "url": "/webenv-assignment3-client-dist/js/app.db9efafe.js"
+  },
+  {
+    "revision": "917a1ff8ab620f0beb45",
+    "url": "/webenv-assignment3-client-dist/js/chunk-0b12526b.98848b29.js"
+  },
+  {
+    "revision": "396b3e71d6f0275e1ebb",
+    "url": "/webenv-assignment3-client-dist/js/chunk-1ddd5a8a.fa7a0e89.js"
+  },
+  {
+    "revision": "56013ea6b697f17ec27a",
+    "url": "/webenv-assignment3-client-dist/js/chunk-296a3085.a4fc47c7.js"
+  },
+  {
+    "revision": "1c1d79c049a93acf4b67",
+    "url": "/webenv-assignment3-client-dist/js/chunk-2d0c031e.ddf8c974.js"
+  },
+  {
+    "revision": "826d04e7c11e9a4c21f7",
+    "url": "/webenv-assignment3-client-dist/js/chunk-2d0d6d5d.bb2347c0.js"
+  },
+  {
+    "revision": "63ffc8dc1e43b1b548fc",
+    "url": "/webenv-assignment3-client-dist/js/chunk-2d2100db.70309e3b.js"
+  },
+  {
+    "revision": "dabc5434af65e0740113",
+    "url": "/webenv-assignment3-client-dist/js/chunk-2d217357.156bb90c.js"
+  },
+  {
+    "revision": "f39ae271bec8f32ed4f3",
+    "url": "/webenv-assignment3-client-dist/js/chunk-2d22d433.cfd7b5a6.js"
+  },
+  {
+    "revision": "b01841e5b964aa17c76d",
+    "url": "/webenv-assignment3-client-dist/js/chunk-2d22d746.540939ab.js"
+  },
+  {
+    "revision": "2f723fe8798b790dfcb1",
+    "url": "/webenv-assignment3-client-dist/js/chunk-2d2304b0.4325e7d7.js"
+  },
+  {
+    "revision": "18e133d8c9cc570550b1",
+    "url": "/webenv-assignment3-client-dist/js/chunk-3615582d.c9957159.js"
+  },
+  {
+    "revision": "f3d3e84b0153dcd0485c",
+    "url": "/webenv-assignment3-client-dist/js/chunk-5f9f11b2.7aa85853.js"
+  },
+  {
+    "revision": "48c4be7523ddddec6a9c",
+    "url": "/webenv-assignment3-client-dist/js/chunk-e9e4a9b2.f4220234.js"
+  },
+  {
+    "revision": "8d097d60ace2838f3ba6",
+    "url": "/webenv-assignment3-client-dist/js/chunk-vendors.7364414f.js"
+  },
+  {
+    "revision": "b38d2fe0c20cd6f5415e3c26922d78ef",
+    "url": "/webenv-assignment3-client-dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/webenv-assignment3-client-dist/robots.txt"
+  }
+]);
